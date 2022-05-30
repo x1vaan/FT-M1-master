1b36fc39e4949745ada5a2e4a01ed11e69ccde2b
x1vaan <Ivanx467@gmail.com>
2022-05-30 16:07:58 -0300

    Estructura De Datos III

--- 06-EstructuraDeDatos-III/homework/homework.js
+++ 06-EstructuraDeDatos-III/homework/homework.js
@@ -1,5 +1,7 @@
 "use strict";
 
+const { convertErrorToString } = require("@11ty/eleventy/src/EleventyErrorUtil");
+
 /*
  Implementar la clase BinarySearchTree, definiendo los siguientes métodos recursivos:
   - size: retorna la cantidad total de nodos del árbol
@@ -11,7 +13,97 @@
   El ábrol utilizado para hacer los tests se encuentra representado en la imagen bst.png dentro del directorio homework.
 */
 
-function BinarySearchTree() {}
+function BinarySearchTree(value) {
+  this.value = value,
+  this.left = null,
+  this.right = null;
+}
+
+BinarySearchTree.prototype.size = function(){ 
+      if(!this.value) return 0;
+       var suma = 0;
+       suma++
+       if(this.left && this.right){
+         return suma + this.left.size() + this.right.size();
+       }
+       if(this.left){
+         return suma + this.left.size();
+       }
+       if(this.right){
+         return suma + this.right.size()
+       }
+       return suma;
+}
+
+BinarySearchTree.prototype.insert = function(value){
+    if(value < this.value){
+        if(!this.left){
+          this.left = new BinarySearchTree(value);
+          return;
+        }
+           var raiz = this.left ;       
+        return raiz.insert(value)
+    }
+    else {value > this.value}{
+      if(!this.right){
+        this.right = new BinarySearchTree(value)
+        return;
+      }
+      var raiz2 = this.right;
+      return raiz2.insert(value)
+    }
+}
+
+BinarySearchTree.prototype.contains = function(value){
+      if(this.value ===  value){
+        return true;
+      }
+     else if(this.left && this.right){
+        return this.left.contains(value) || this.right.contains(value)
+     }
+    else if(this.left){
+       return this.left.contains(value);
+     }
+    else if(this.right){
+       return this.right.contains(value);
+     }
+     return false;
+}
+
+BinarySearchTree.prototype.depthFirstForEach = function(cb, traversal){
+  if (traversal === 'in-order') {
+    if(this.left) this.left.depthFirstForEach(cb, traversal);
+     cb(this.value);
+     if(this.right) this.right.depthFirstForEach(cb, traversal);
+     return;
+   }
+     else if (traversal === 'pre-order'){
+      cb(this.value);
+      if(this.left) this.left.depthFirstForEach(cb, traversal);
+     if(this.right) this.right.depthFirstForEach(cb, traversal);
+     return;
+     }
+     else if(traversal === 'post-order'){
+      if(this.left) this.left.depthFirstForEach(cb, traversal);
+      if(this.right) this.right.depthFirstForEach(cb, traversal);
+      cb(this.value);
+      return;
+     }
+     if(this.left) this.left.depthFirstForEach(cb, traversal);
+     cb(this.value);
+     if(this.right) this.right.depthFirstForEach(cb, traversal);
+}
+
+BinarySearchTree.prototype.breadthFirstForEach = function(cb, array = []){
+  cb(this.value);
+  array.push(this.value);
+   if(this.left && this.right){
+     return this.left.breadthFirstForEach(cb,array) && this.right.breadthFirstForEach(cb, array);
+   }
+   if(this.left) return this.left.breadthFirstForEach(cb,array);
+   if(this.right) return this.right.breadthFirstForEach(cb,array);
+   return;
+}
 
 // No modifiquen nada debajo de esta linea
 // --------------------------------
